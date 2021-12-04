9884916984c082d1256d7c5a948d13fe271e4a5b
chellesjord <m.kayejordan@gmail.com>
2021-12-04 17:43:39 -0600

    structured time blocks in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-//append to date to p
-var currentDay = moment().format("MMMM Do");
-document.getElementById("date").innerHTML = currentDay;
+//if document ready. Won't start until it's loaded
+$(document).ready(function () {
+    //append to date to p
+    var currentDay = moment().format("MMMM Do");
+    document.getElementById("date").innerHTML = currentDay;
+})
+
